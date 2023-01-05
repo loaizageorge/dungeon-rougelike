9ecc01ed46593730e1ea90eacd28f43668ae6627
George <loaizageorge@gmail.com>
2023-01-05 15:54:29 -0500

    move visibility to constant

--- src/Utils/constants.ts
+++ src/Utils/constants.ts
@@ -1,4 +1,5 @@
 // grid is 20 * 20 cells
 export const MAX_BOUNDARY = 20;
 
-export const VISIBILITY = 5;
+// The hero will have a area of visibility of 2 * VISIBILITY + 1
+export const VISIBILITY = 2;
